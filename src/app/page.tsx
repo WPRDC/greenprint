a11ac333ff5d95d276272622122b5647903d2dd1
Steve Saylor <steven.saylor@pitt.edu>
2024-02-21 12:28:44 -0500

    added landing page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home({
         <Map selectedParcel={parcelID}></Map>
       </div>
       <div className="relative w-full max-w-md overflow-auto border-l border-blue-800">
-        {!!parcelID && (
+        {!!parcelID ? (
           <Suspense
             key={parcelID}
             fallback={
@@ -39,6 +39,55 @@ export default function Home({
           >
             <PropertyDashboard parcelID={parcelID} />
           </Suspense>
+        ) : (
+          <div className="p-4">
+            <div className="font-roboto mb-3 text-4xl font-medium">
+              Welcome to Greenprint
+            </div>
+            <div>
+              <p>
+                To better inform land use decisions and ownership considerations
+                ALT is developing an intersectional analysis of parcel status of
+                the Greater Pittsburgh Region, with a focus on key regional
+                initiatives. Urban greening is a multi-benefit solution to many
+                of the issues facing the region, such as landslides, water
+                management and combined sewer overflows; which have arisen from
+                inappropriate development and poor and aging infrastructure.
+                This “Greenprint” will emphasize greenspaces as solutions,
+                recognizing that all forms of urban greening can positively
+                contribute to the overall health and benefit of the area. The
+                Greenprint will guide ALT, policy makers, and the community on
+                the best uses of parcels to improve comprehensive regional
+                planning and decision-making.
+              </p>
+              <ul className="mt-4 list-inside list-disc space-y-2">
+                <li>
+                  The Greenprint project is a full partnership between{" "}
+                  <a href="https://alleghenylandtrust.org/">
+                    Allegheny Land Trust
+                  </a>{" "}
+                  and{" "}
+                  <a href="https://www.wprdc.org">
+                    the Western Pennsylvania Regional Data Center
+                  </a>
+                  .
+                </li>
+                <li>
+                  We (ALT and WPRDC) have worked together to obtain the datasets
+                  for this project.{" "}
+                </li>
+                <li>
+                  This is now a &ldquo;living&rdquo; project which will update
+                  and evolve over time. You can view the code and submit issues
+                  at the{" "}
+                  <a href="https://github.com/WPRDC/greenprint">
+                    Github Repository
+                  </a>
+                  .
+                </li>
+              </ul>
+            </div>
+          </div>
         )}
       </div>
     </main>
